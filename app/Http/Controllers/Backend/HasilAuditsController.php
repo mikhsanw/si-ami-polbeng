bdb91b968dba14b8112b52d5b92a625fba81aae4
Muhammad Ikhsan Wibowo <ikhsan.flip@gmail.com>
2026-06-09 15:20:57 +0700

    tes

--- app/Http/Controllers/Backend/HasilAuditsController.php
+++ app/Http/Controllers/Backend/HasilAuditsController.php
@@ -558,7 +558,6 @@ class HasilAuditsController extends Controller
 
     public function deleteFile(Request $request)
     {
-        dd($request->all());
         // Pastikan request adalah AJAX dan user memiliki izin
         if (! $request->ajax() || ! auth()->user()->hasRole('Auditee')) { // Sesuaikan role atau permission
             return response()->json(['success' => false, 'message' => 'Akses ditolak.'], 403);
